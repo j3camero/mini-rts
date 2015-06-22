95edb7fae581b0c9384dadeeb149aefd44164e1d
Jeff Cameron <cameron.jp@gmail.com>
2015-06-21 20:44:07 -0400

    Added constraints to perlin terrain generation to make adjacent chunks fit seamlessly together.

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ var io = require('socket.io')(http);
 var chunk_size = 128;
 var chunks = {};
 var perlin_spectrum = [0, 0, 1, 2, 4, 8, 16, 32];
-var perlin_constraints = new Array(perlin_spectrum.length);
+var perlin_constraints = [];
+for (var i = 0; i < perlin_spectrum.length; ++i) {
+    perlin_constraints.push({});
+}
 
 function ZeroMatrix(size) {
     var matrix = [];
@@ -37,22 +40,112 @@ function Key2D(x, y) {
     return x + ':' + y;
 }
 
-function LoadConstraint(layer, i, j, x, y, remove, replacement) {
-    // Calculate abs x,y
-    var abs_x = i * chunk_size + x;
-    var abs_y = j * chunk_size + j;
-    var key = Key2D(abs_x, abs_y);
-    var cons = perlin_constraints[layer];
-    if (key in cons) {
-	var val = cons[key];
-	if (remove) {
-	    delete cons[key];
-	}
-	return val;
-    } else {
-	return null;
-    }
+function ChunkExists(i, j) {
+    return Key2D(i, j) in chunks;
 }
+
+function LoadAndUpdateConstraints(target_matrix, i, j, layer, p) {
+    var c = perlin_constraints[layer];
+    // Look for adjacent chunks.
+    var cn = ChunkExists(i, j-1);
+    var cs = ChunkExists(i, j+1);
+    var ce = ChunkExists(i+1, j);
+    var cw = ChunkExists(i-1, j);
+    var cne = ChunkExists(i+1, j-1);
+    var cnw = ChunkExists(i-1, j-1);
+    var cse = ChunkExists(i+1, j+1);
+    var csw = ChunkExists(i-1, j+1);
+    var ip = i * p;
+    var jp = j * p;
+    // North edge.
+    if (cn) {
+	for (var k = 1; k < p; ++k) {
+	    var key = Key2D(ip+k,jp);
+	    target_matrix[k][0] = c[key];
+	    delete c[key];
+	}
+    } else {
+	for (var k = 1; k < p; ++k) {
+	    c[Key2D(ip+k,jp)] = target_matrix[k][0];
+	}
+    }
+    // South edge.
+    if (cs) {
+	for (var k = 1; k < p; ++k) {
+	    var key = Key2D(ip+k,jp+p);
+	    target_matrix[k][p] = c[key];
+	    delete c[key];
+	}
+    } else {
+	for (var k = 1; k < p; ++k) {
+	    c[Key2D(ip+k,jp+p)] = target_matrix[k][p];
+	}
+    }
+    // West edge.
+    if (cw) {
+	for (var k = 1; k < p; ++k) {
+	    var key = Key2D(ip,jp+k);
+	    target_matrix[0][k] = c[key];
+	    delete c[key];
+	}
+    } else {
+	for (var k = 1; k < p; ++k) {
+	    c[Key2D(ip,jp+k)] = target_matrix[0][k];
+	}
+    }
+    // East edge.
+    if (ce) {
+	for (var k = 1; k < p; ++k) {
+	    var key = Key2D(ip+p,jp+k);
+	    target_matrix[p][k] = c[key];
+	    delete c[key];
+	}
+    } else {
+	for (var k = 1; k < p; ++k) {
+	    c[Key2D(ip+p,jp+k)] = target_matrix[p][k];
+	}
+    }
+    // North-east corner. (p,0)
+    if (cn && ce && cne) {
+	var key = Key2D(ip+p,jp);
+	target_matrix[p][0] = c[key];
+	delete c[key];
+    } else if ((!cn) && (!ce) && (!cne)) {
+	c[Key2D(ip+p,jp)] = target_matrix[p][0];
+    } else {
+	target_matrix[p][0] = c[Key2D(ip+p,jp)];
+    }
+    // South-east corner. (p,p)
+    if (cs && ce && cse) {
+	var key = Key2D(ip+p,jp+p);
+	target_matrix[p][p] = c[key];
+	delete c[key];
+    } else if ((!cs) && (!ce) && (!cse)) {
+	c[Key2D(ip+p,jp+p)] = target_matrix[p][p];
+    } else {
+	target_matrix[p][p] = c[Key2D(ip+p,jp+p)];
+    }
+    // North-west corner. (0,0)
+    if (cn && cw && cnw) {
+	var key = Key2D(ip,jp);
+	target_matrix[0][0] = c[key];
+	delete c[key];
+    } else if ((!cn) && (!cw) && (!cnw)) {
+	c[Key2D(ip,jp)] = target_matrix[0][0];
+    } else {
+	target_matrix[0][0] = c[Key2D(ip,jp)];
+    }
+    // South-west corner. (0,p)
+    if (cs && cw && csw) {
+	var key = Key2D(ip,jp+p);
+	target_matrix[0][p] = c[key];
+	delete c[key];
+    } else if ((!cs) && (!cw) && (!csw)) {
+	c[Key2D(ip,jp+p)] = target_matrix[0][p];
+    } else {
+	target_matrix[0][p] = c[Key2D(ip,jp+p)];
+    }
+} // LoadAndUpdateConstraints
 
 function GenerateChunk(i, j) {
     var terrain = ZeroMatrix(chunk_size);
@@ -64,6 +157,7 @@ function GenerateChunk(i, j) {
 	var resolution = Math.pow(2, layer);
 	var matrix_size = chunk_size / resolution + 1;
 	var layer_matrix = UniformRandomMatrix(matrix_size, magnitude);
+	LoadAndUpdateConstraints(layer_matrix, i, j, layer, matrix_size - 1);
 	for (var x = 0; x < chunk_size; ++x) {
 	    var mx = Math.floor(x / resolution);
 	    var h = (x % resolution) / resolution;
@@ -95,7 +189,7 @@ function GenerateChunk(i, j) {
     }
     rle.push(rle_count);
     return {tiles: tiles, rle: rle};
-}
+} // GenerateChunk
 
 function GetChunk(i, j) {
     var key = Key2D(i, j);
