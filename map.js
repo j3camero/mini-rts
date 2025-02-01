c94308da22dc004bd5318b0a5e3ccf4e7f015bba
Jeff Cameron <jeff@Jeffs-MBP.ht.home>
2025-01-31 19:27:02 -0500

    Mess with the map generation parameters.

--- map.js
+++ map.js
@@ -3,16 +3,16 @@ const fs = require('fs');
 const { createNoise2D } = require('simplex-noise');
 
 function HeightToColor(h) {
-  if (h < -0.5) return '#1D4875';
+  if (h < -0.6) return '#1D4875';
   if (h < -0.4) return '#1A528C';
   if (h < -0.3) return '#1E69B3';
   if (h < -0.2) return '#3085DA';
   if (h < -0.1) return '#2897FF';
-  if (h < 0.3) return '#9FB840';
-  if (h < 0.5) return '#34782F';
-  if (h < 0.6) return '#888888';
-  if (h < 0.7) return '#6C6C6C';
-  return '#000000';
+  if (h < 0.2) return '#9FB840';
+  if (h < 0.4) return '#34782F';
+  if (h < 0.5) return '#888888';
+  if (h < 0.6) return '#6C6C6C';
+  return '#555555';
 }
 
 const w = 1920;
@@ -23,9 +23,18 @@ const noise = createNoise2D();
 for (let i = 0; i < w; i++) {
   for (let j = 0; j < h; j++) {
     const h = (
-      0.6 * noise(i / 400, j / 400) +
-      0.3 * noise(i / 200, j / 200) +
-      0.1 * noise(i / 100, j / 100)
+      // 32/63 * noise(i / 640, j / 640) +
+      // 16/63 * noise(i / 320, j / 320) +
+      // 8/63 * noise(i / 160, j / 160) +
+      // 4/63 * noise(i / 80, j / 80) +
+      // 2/63 * noise(i / 40, j / 40) +
+      // 1/63 * noise(i / 20, j / 20)
+      6/21 * noise(i / 640, j / 640) +
+      5/21 * noise(i / 320, j / 320) +
+      4/21 * noise(i / 160, j / 160) +
+      3/21 * noise(i / 80, j / 80) +
+      2/21 * noise(i / 40, j / 40) +
+      1/21 * noise(i / 20, j / 20)
     );
     const color = HeightToColor(h);
     ctx.fillStyle = color;
